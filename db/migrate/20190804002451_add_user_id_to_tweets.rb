755bf734ea7d681d85dcbb03fc724a111a02432b
KengoNagata <majorboy47@gmail.com>
2019-08-04 09:33:05 +0900

    a

---
+++ db/migrate/20190804002451_add_user_id_to_tweets.rb
@@ -0,0 +1,5 @@
+class AddUserIdToTweets < ActiveRecord::Migration[5.2]
+  def change
+    add_column :tweets, :userid, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_03_234655) do
+ActiveRecord::Schema.define(version: 2019_08_04_002451) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_08_03_234655) do
     t.text "image"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "userid"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
